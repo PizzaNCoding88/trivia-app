e144f55f93e7aebc32122d505270067f55c517af
PizzaNCoding88 <umbertonardiello@gmail.com>
2025-05-22 21:43:38 +0100

    fonts adjusted and main page created in page

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,16 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Aboreto, Alkatra } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const aboreto = Aboreto({
   subsets: ["latin"],
+  weight: "400",
+  variable: "--font-aboreto",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const alkatra = Alkatra({
   subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-main",
 });
 
 export const metadata = {
@@ -19,7 +21,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${aboreto.variable} ${alkatra.variable}`}>
         {children}
       </body>
     </html>
--- app/page.js
+++ app/page.js
@@ -1,12 +1,31 @@
-import Image from "next/image";
 import styles from "./page.module.css";
-import { IconButton } from "@mui/material";
-import AddIcon from "@mui/icons-material/Add";
+import { Button } from "@mui/material";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <h1>Test h1</h1>
+      <div className={styles.container}>
+        <h1 className={styles.title}>
+          Welcome to the OpenTDB Trivia Game. Please choose your options in the
+          next steps and get ready to play
+        </h1>
+        <Button
+          sx={{
+            textTransform: "capitalize",
+            borderColor: "var(--background)",
+            borderWidth: "2px",
+            boxShadow: "4px 4px 10px var(--background)",
+            backgroundColor: "var(--buttons-bg)",
+            width: "30vw",
+            fontFamily: "var(--font-main)",
+            color: "var(--background)",
+          }}
+          size="large"
+          variant="outlined"
+        >
+          Continue
+        </Button>
+      </div>
     </div>
   );
 }
