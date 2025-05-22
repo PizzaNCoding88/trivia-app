35ba4d1f660208fbe4a914a5a6dc001585884763
PizzaNCoding88 <umbertonardiello@gmail.com>
2025-05-22 20:57:59 +0100

    font tests

--- app/page.js
+++ app/page.js
@@ -4,5 +4,9 @@ import { IconButton } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 
 export default function Home() {
-  return <div className={styles.page}></div>;
+  return (
+    <div className={styles.page}>
+      <h1>Test h1</h1>
+    </div>
+  );
 }
