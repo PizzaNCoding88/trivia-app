772ab4af8acfb39469a3fa71149c4eae8bd3eac6
PizzaNCoding88 <umbertonardiello@gmail.com>
2025-05-23 16:34:48 +0100

    test

--- app/mainPage/Main.js
+++ app/mainPage/Main.js
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./Main.module.css";
 import { Button } from "@mui/material";
 import Link from "next/link";
+import Buttons from "@/components/Buttons";
 
 const Main = () => {
   return (
@@ -12,22 +13,7 @@ const Main = () => {
           next steps and get ready to play
         </h1>
         <Link href="/questions">
-          <Button
-            sx={{
-              textTransform: "capitalize",
-              borderColor: "var(--background)",
-              borderWidth: "2px",
-              boxShadow: "4px 4px 10px var(--background)",
-              backgroundColor: "var(--buttons-bg)",
-              width: "30vw",
-              fontFamily: "var(--font-main)",
-              color: "var(--background)",
-            }}
-            size="large"
-            variant="outlined"
-          >
-            Continue
-          </Button>
+          <Buttons name={"Continue"} />
         </Link>
       </div>
     </div>
--- app/questions/page.js
+++ app/questions/page.js
@@ -1,8 +1,144 @@
+"use client";
 import React from "react";
 import styles from "./page.module.css";
+import {
+  Select,
+  MenuItem,
+  InputLabel,
+  FormControl,
+  Typography,
+<<<<<<< HEAD
+  Button,
+=======
+>>>>>>> 9dcc9103f06c0a05ced8cd937b2fddca1753f38a
+} from "@mui/material";
+import { useState } from "react";
+import { categories } from "../../public/categories/categories";
+import { difficulties } from "@/public/difficulties/difficulties";
+<<<<<<< HEAD
+import Buttons from "@/components/Buttons";
+=======
+>>>>>>> 9dcc9103f06c0a05ced8cd937b2fddca1753f38a
 
 const QuestionsPage = () => {
-  return <div className={styles.page}></div>;
+  const [questionsQuantity, setQuestionsQuantity] = useState("");
+  const [category, setCategory] = useState("");
+  const [difficulty, setDifficulty] = useState("");
+  const [style, setStyle] = useState("");
+  const types = [
+    { value: "multiple", label: "Multiple Choice" },
+    { value: "boolean", label: "True / False" },
+  ];
+  return (
+    <div className={styles.page}>
+      <div>
+        <Typography
+          sx={{
+            fontFamily: "var(--font-main)",
+            fontSize: 30,
+            textAlign: "center",
+          }}
+        >
+          How many questions would you like to go for?
+        </Typography>
+        <FormControl fullWidth variant="filled">
+          <InputLabel>Number of questions</InputLabel>
+
+          <Select
+            value={questionsQuantity}
+            onChange={(e) => setQuestionsQuantity(e.target.value)}
+          >
+            {Array.from({ length: 50 }, (_, i) => (
+              <MenuItem key={i + 1} value={i + 1}>
+                {i + 1}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+      <div>
+        <Typography
+          sx={{
+            fontFamily: "var(--font-main)",
+            fontSize: 30,
+            textAlign: "center",
+          }}
+        >
+          What's your favourite category?
+        </Typography>
+        <FormControl fullWidth variant="filled">
+          <InputLabel>Choose category</InputLabel>
+
+          <Select
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            {categories.map((category) => (
+              <MenuItem key={category.id} value={category.id}>
+                {category.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+      <div>
+        <Typography
+          sx={{
+            fontFamily: "var(--font-main)",
+            fontSize: 30,
+            textAlign: "center",
+          }}
+        >
+          What's the difficulty you'd like to go for?
+        </Typography>
+        <FormControl fullWidth variant="filled">
+          <InputLabel>Choose Difficulty</InputLabel>
+
+          <Select
+            value={difficulty}
+            onChange={(e) => setDifficulty(e.target.value)}
+          >
+            {difficulties.map((difficulty, i) => (
+              <MenuItem key={i} value={difficulty.value}>
+                {difficulty.label}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        {console.log(questionsQuantity, category, difficulty)}
+      </div>
+      <div>
+        <Typography
+          sx={{
+            fontFamily: "var(--font-main)",
+            fontSize: 30,
+            textAlign: "center",
+          }}
+        >
+          Do you prefer multiple choice or true/false style of trivia?
+        </Typography>
+        <FormControl fullWidth variant="filled">
+          <InputLabel>Choose Style</InputLabel>
+
+          <Select value={style} onChange={(e) => setStyle(e.target.value)}>
+            {types.map((type, i) => (
+              <MenuItem key={i} value={type.value}>
+                {type.label}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+<<<<<<< HEAD
+      </div>
+      <div className={styles.buttonsContainer}>
+        <Buttons name={"Reset Options"} />
+        <Buttons name={"Start Game"} />
+=======
+        {console.log(questionsQuantity, category, difficulty, style)}
+>>>>>>> 9dcc9103f06c0a05ced8cd937b2fddca1753f38a
+      </div>
+    </div>
+  );
 };
 
 export default QuestionsPage;
