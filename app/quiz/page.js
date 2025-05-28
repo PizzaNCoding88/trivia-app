0bebb5e6769e70b533b05835e9e83263b3590dd9
PizzaNCoding88 <umbertonardiello@gmail.com>
2025-05-28 14:14:51 +0100

    quiz page started

--- app/quiz/page.js
+++ app/quiz/page.js
@@ -2,35 +2,46 @@
 import React, { useState } from "react";
 import { useQuiz } from "../context/QuizContext";
 import { useRouter } from "next/navigation";
-import { Typography } from "@mui/material";
+import { Button, Typography, ButtonGroup } from "@mui/material";
+import { shuffleArray } from "../utils/shuffleArray";
+import styles from "./page.module.css";
 
 const QuizPage = () => {
   const { questions } = useQuiz();
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  let quiz = questions.results;
-  const answers = [
-    ...quiz[currentIndex].incorrect_answers,
-    quiz[currentIndex].correct_answer,
-  ];
   if (questions && questions.results && questions.results.length > 0) {
     let quiz = questions.results;
+    const answers = [
+      ...quiz[currentIndex].incorrect_answers,
+      quiz[currentIndex].correct_answer,
+    ];
     console.log(quiz[currentIndex]);
     console.log(answers);
-  } else {
-    console.log("No data fetched");
-  }
-
-  return (
-    <div>
-      {questions && questions.results && questions.results.length > 0 ? (
+    const shuffledAnswers = shuffleArray(answers);
+    return (
+      <div className={styles.container}>
         <Typography>{quiz[currentIndex].question}</Typography>
-      ) : (
-        <Typography>Error with fetching</Typography>
-      )}
-      {<div>Test</div>}
-    </div>
-  );
+        <ButtonGroup
+          sx={{
+            display: "grid",
+            gridTemplateColumns: "200px 200px",
+            gap: "1rem",
+          }}
+        >
+          {shuffledAnswers.map((answer, i) => (
+            <Button
+              key={i}
+              variant="contained"
+              sx={{ borderRadius: "4px !important" }}
+            >
+              {answer}
+            </Button>
+          ))}
+        </ButtonGroup>
+      </div>
+    );
+  }
 };
 
 export default QuizPage;
