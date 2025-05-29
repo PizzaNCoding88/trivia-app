1c07ff54585fce3043d4e795c113bbbca23464c7
PizzaNCoding88 <umbertonardiello@gmail.com>
2025-05-29 17:31:21 +0100

    theme 1.0

---
+++ app/utils/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#6233ca",
+    },
+    secondary: {
+      main: "#001695",
+    },
+  },
+});
+//import font to download from gmail
+export default theme;
